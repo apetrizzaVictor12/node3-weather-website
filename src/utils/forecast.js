2561e83554bf0670b75b7fde2d6e45d4b4121e14
Anthony Petrizza <apetrizza@VICTOR12.LOCAL>
2019-12-06 16:06:43 -0500

    Added high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,7 +18,11 @@ const forecast = (lat, long, callback) => {
         body.daily.data[0].summary +
           " It is currently " +
           body.currently.temperature +
-          " degrees outside. There is a " +
+          " degrees outside. There is a high today of " +
+          body.daily.data[0].temperatureHigh +
+          ", with a low of " +
+          body.daily.data[0].temperatureLow +
+          ". There is a " +
           body.currently.precipProbability +
           "% chance of rain"
       );
